72b5c7cdc32376df89083bd6a48f65a8ac9f5e3d
João Vitor Moreira <joaoitagv@gmail.com>
2023-05-10 12:08:44 -0300

    JS

---
+++ teste.js
@@ -0,0 +1,30 @@
+console.log("Hello World!")
+
+let nome = "João Vitor"
+
+const sobrenome = "Moreira"
+
+console.log(nome + " " + sobrenome)
+
+nome = "Sabrina"
+
+console.log(nome + " " + sobrenome)
+
+let numero1 = 30
+let numero2 = 10
+
+console.log(numero1 +numero2)
+
+function dividir (num1, num2){
+    return num1 / num2
+}
+
+console.log(dividir(10, 5))
+
+function tabuada(num){
+    for(let contador = 1; contador <=10; contador ++){
+        console.log(`${num1} x ${contador} = ${num1 * contador}`)
+    }
+}
+
+tabuada(5)
